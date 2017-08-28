96a0295ed306c510fd104aa348db52f9a40bcc5f
Razzer <Razzer_05@hotmail.com>
2017-08-28 15:52:37 +0100

    Latest

---
+++ GUIFilesRenamer/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace GUIFilesRenamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 38);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(725, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox2.Location = new System.Drawing.Point(36, 106);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(487, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(32, 75);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name of TV show";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox3.Location = new System.Drawing.Point(547, 106);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(87, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Location = new System.Drawing.Point(543, 75);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Season";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(663, 102);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Rename";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(792, 38);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 28);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Browse";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label3.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label3.Location = new System.Drawing.Point(36, 15);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(214, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Folder containing files to rename";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.richTextBox1.Location = new System.Drawing.Point(36, 164);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(487, 514);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.richTextBox2.Location = new System.Drawing.Point(547, 164);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(344, 514);
+            this.richTextBox2.TabIndex = 11;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(908, 736);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "File Renamer by Razzer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ GUIFilesRenamer/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUIFilesRenamer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+                richTextBox1.Text = "";
+                richTextBox2.Text = "";
+
+
+                var fileEntries = Directory.EnumerateFiles(textBox1.Text);
+
+                foreach (var file in fileEntries)
+                {
+                    FileInfo fileInfo = new FileInfo(file);
+                    if (richTextBox1.Text == "")
+                        richTextBox1.Text = fileInfo.Name;
+                    else
+                    {
+                        richTextBox1.Text = richTextBox1.Text + "\n" + fileInfo.Name;
+                    }
+                    
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            const string title = "Error";
+            var path = this.textBox1.Text;
+
+            if (path != "" && textBox2.Text != "" && textBox3.Text != "")
+            {
+                
+                var fileEntries = Directory.EnumerateFiles(path);
+
+                var i = 1;
+
+                bool shownError = false;
+
+
+                string[] mediaExtensions = {
+                    ".PNG", ".JPG", ".JPEG", ".BMP", ".GIF",
+                    ".WAV", ".MID", ".MIDI", ".WMA", ".MP3", ".OGG", ".RMA",
+                    ".AVI", ".MP4", ".DIVX", ".WMV", ".mkv"
+                };
+
+
+                foreach (var item in fileEntries)
+                {
+
+                    FileInfo file = new FileInfo(item);
+                    if (mediaExtensions.Contains(file.Extension, StringComparer.OrdinalIgnoreCase))
+                    {
+
+
+                        string text = "";
+                        bool foundEpisodeNum = false;
+                        var i1 = 0;
+                        var fileNameToSearch = file.Name.Substring(i1);
+                        while (!foundEpisodeNum)
+                        {
+                            
+                            i1 = fileNameToSearch.IndexOf("E", StringComparison.InvariantCultureIgnoreCase);
+                            text = fileNameToSearch.Substring(i1 + 1, 2);
+                            foundEpisodeNum = Char.IsNumber(Convert.ToChar(text.Substring(0, 1)));
+                            
+                            if (i1 == -1)
+                            {
+                                string errorMsg = "Could not find episode number in file name " + file.Name;
+                                MessageBox.Show(errorMsg);
+                                return;
+                            }
+                            fileNameToSearch = fileNameToSearch.Substring(i1+2);
+                        }
+                        if (!Char.IsNumber(Convert.ToChar(text.Substring(1, 1))))
+                        {
+                            text = text.Substring(0, 1);
+                        }
+                        if (text.Length <= 1)
+                        {
+                            text = "0" + text;
+                        }
+
+                        //MessageBox.Show(Convert.ToString(i1), "error", MessageBoxButtons.OK);
+                        if (textBox3.Text.Length <= 1)
+                            textBox3.Text = "0" + textBox3.Text;
+
+                        textBox2.Text = textBox2.Text.Substring(0, 1).ToUpper() + textBox2.Text.Substring(1);
+
+                        var fileName = path + "\\" + textBox2.Text + " S" + textBox3.Text + "E" + text + file.Extension.ToLower();
+
+                        try
+                        {
+                            file.MoveTo(fileName);
+                            if (richTextBox2.Text == "")
+                                richTextBox2.Text = textBox2.Text + " S" + textBox3.Text + "E" + text + file.Extension.ToLower();
+                            else
+                            {
+                                richTextBox2.Text = richTextBox2.Text + "\n" + textBox2.Text + " S" + textBox3.Text + "E" + text + file.Extension.ToLower();
+                            }
+                        }
+                        catch (Exception exception)
+                        {
+                            Console.WriteLine(exception);
+                            const string errorMsg =
+                                "An error occured and didn't rename all files, make sure there isn't duplicate episodes with the same episode number.";
+                            if (!shownError)
+                                MessageBox.Show(errorMsg, title, MessageBoxButtons.OK);
+                            shownError = true;
+                        }
+
+                        i++;
+                    }
+                    else
+                    {
+                        string errorMsg = "Could not rename file " + file.Name +
+                                                " as the extension is not a media type";
+                        MessageBox.Show(errorMsg);
+                    }
+                }
+
+            }
+            else
+            {
+                const string errorMsg = "Fill all the boxes";
+                MessageBox.Show(errorMsg, title, MessageBoxButtons.OK);
+            }
+
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+    }
+}
