d74ce095f4e7333ef6c0261902bd51ddaea8c115
Razzer <Razzer_05@hotmail.com>
2017-08-28 15:54:06 +0100

    start of recursive file rename

--- GUIFilesRenamer/Form1.Designer.cs
+++ GUIFilesRenamer/Form1.Designer.cs
@@ -34,11 +34,13 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.renameButton = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // folderBrowserDialog1
@@ -47,69 +49,74 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(36, 38);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Location = new System.Drawing.Point(13, 38);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(725, 22);
+            this.textBox1.Size = new System.Drawing.Size(774, 22);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
             this.textBox2.BackColor = System.Drawing.SystemColors.Control;
-            this.textBox2.Location = new System.Drawing.Point(36, 106);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Location = new System.Drawing.Point(16, 124);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(487, 22);
             this.textBox2.TabIndex = 2;
+            this.textBox2.Visible = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.label1.Location = new System.Drawing.Point(32, 75);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(13, 100);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(119, 17);
+            this.label1.Size = new System.Drawing.Size(197, 17);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Name of TV show";
+            this.label1.Text = "Rename TV show Episodes to";
+            this.label1.Visible = false;
             // 
             // textBox3
             // 
             this.textBox3.BackColor = System.Drawing.SystemColors.Control;
-            this.textBox3.Location = new System.Drawing.Point(547, 106);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Location = new System.Drawing.Point(511, 124);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(87, 22);
             this.textBox3.TabIndex = 4;
+            this.textBox3.Visible = false;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.label2.Location = new System.Drawing.Point(543, 75);
+            this.label2.Location = new System.Drawing.Point(508, 100);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(56, 17);
             this.label2.TabIndex = 5;
             this.label2.Text = "Season";
+            this.label2.Visible = false;
             // 
-            // button2
+            // renameButton
             // 
-            this.button2.Location = new System.Drawing.Point(663, 102);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 28);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Rename";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.renameButton.Enabled = false;
+            this.renameButton.Location = new System.Drawing.Point(792, 100);
+            this.renameButton.Margin = new System.Windows.Forms.Padding(4);
+            this.renameButton.Name = "renameButton";
+            this.renameButton.Size = new System.Drawing.Size(100, 28);
+            this.renameButton.TabIndex = 7;
+            this.renameButton.Text = "Rename";
+            this.renameButton.UseVisualStyleBackColor = true;
+            this.renameButton.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(792, 38);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Location = new System.Drawing.Point(795, 35);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(100, 28);
             this.button3.TabIndex = 8;
@@ -121,8 +128,8 @@
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.label3.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.label3.Location = new System.Drawing.Point(36, 15);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(10, 9);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(214, 17);
@@ -132,34 +139,58 @@
             // richTextBox1
             // 
             this.richTextBox1.BackColor = System.Drawing.SystemColors.Control;
-            this.richTextBox1.Location = new System.Drawing.Point(36, 164);
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 154);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(487, 514);
+            this.richTextBox1.Size = new System.Drawing.Size(487, 312);
             this.richTextBox1.TabIndex = 10;
             this.richTextBox1.Text = "";
             // 
             // richTextBox2
             // 
             this.richTextBox2.BackColor = System.Drawing.SystemColors.Control;
-            this.richTextBox2.Location = new System.Drawing.Point(547, 164);
-            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox2.Location = new System.Drawing.Point(507, 154);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(344, 514);
+            this.richTextBox2.Size = new System.Drawing.Size(383, 312);
             this.richTextBox2.TabIndex = 11;
             this.richTextBox2.Text = "";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(16, 67);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(139, 21);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "TV Show Options";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(158, 67);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(149, 21);
+            this.checkBox2.TabIndex = 13;
+            this.checkBox2.Text = "Check Sub Folders";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ClientSize = new System.Drawing.Size(908, 736);
+            this.ClientSize = new System.Drawing.Size(908, 489);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.renameButton);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label1);
@@ -167,7 +198,7 @@
             this.Controls.Add(this.textBox1);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -184,11 +215,13 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button renameButton;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
     }
 }
 
--- GUIFilesRenamer/Form1.cs
+++ GUIFilesRenamer/Form1.cs
@@ -14,6 +14,14 @@ namespace GUIFilesRenamer
 {
     public partial class Form1 : Form
     {
+        private string[] mediaExtensions = {
+                    ".PNG", ".JPG", ".JPEG", ".BMP", ".GIF",
+                    ".WAV", ".MID", ".MIDI", ".WMA", ".MP3", ".OGG", ".RMA",
+                    ".AVI", ".MP4", ".DIVX", ".WMV", ".mkv"
+                };
+        private bool shownError = false;
+        const string title = "Error";
+
         public Form1()
         {
             InitializeComponent();
@@ -24,130 +32,48 @@ namespace GUIFilesRenamer
             
         }
 
+        //Browse Button click action
         private void button3_Click(object sender, EventArgs e)
         {
-            
-
             if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
             {
                 textBox1.Text = folderBrowserDialog1.SelectedPath;
-                richTextBox1.Text = "";
-                richTextBox2.Text = "";
-
+                richTextBox1.Text = richTextBox2.Text = "";
 
                 var fileEntries = Directory.EnumerateFiles(textBox1.Text);
 
                 foreach (var file in fileEntries)
                 {
                     FileInfo fileInfo = new FileInfo(file);
-                    if (richTextBox1.Text == "")
-                        richTextBox1.Text = fileInfo.Name;
-                    else
-                    {
-                        richTextBox1.Text = richTextBox1.Text + "\n" + fileInfo.Name;
-                    }
-                    
+                    richTextBox1.Text += fileInfo.Name + "\n";
                 }
+                renameButton.Enabled = true;
             }
         }
 
+        //Rename button click action
         private void button2_Click(object sender, EventArgs e)
         {
-
-            const string title = "Error";
             var path = this.textBox1.Text;
 
-            if (path != "" && textBox2.Text != "" && textBox3.Text != "")
+            if (path != "" && textBox2.Text != "" && textBox3.Text != "" || path != "" && !checkBox1.Checked)
             {
-                
                 var fileEntries = Directory.EnumerateFiles(path);
-
-                var i = 1;
-
-                bool shownError = false;
-
-
-                string[] mediaExtensions = {
-                    ".PNG", ".JPG", ".JPEG", ".BMP", ".GIF",
-                    ".WAV", ".MID", ".MIDI", ".WMA", ".MP3", ".OGG", ".RMA",
-                    ".AVI", ".MP4", ".DIVX", ".WMV", ".mkv"
-                };
-
+                shownError = false;
 
                 foreach (var item in fileEntries)
                 {
-
                     FileInfo file = new FileInfo(item);
                     if (mediaExtensions.Contains(file.Extension, StringComparer.OrdinalIgnoreCase))
                     {
-
-
-                        string text = "";
-                        bool foundEpisodeNum = false;
-                        var i1 = 0;
-                        var fileNameToSearch = file.Name.Substring(i1);
-                        while (!foundEpisodeNum)
-                        {
-                            
-                            i1 = fileNameToSearch.IndexOf("E", StringComparison.InvariantCultureIgnoreCase);
-                            text = fileNameToSearch.Substring(i1 + 1, 2);
-                            foundEpisodeNum = Char.IsNumber(Convert.ToChar(text.Substring(0, 1)));
-                            
-                            if (i1 == -1)
-                            {
-                                string errorMsg = "Could not find episode number in file name " + file.Name;
-                                MessageBox.Show(errorMsg);
-                                return;
-                            }
-                            fileNameToSearch = fileNameToSearch.Substring(i1+2);
-                        }
-                        if (!Char.IsNumber(Convert.ToChar(text.Substring(1, 1))))
-                        {
-                            text = text.Substring(0, 1);
-                        }
-                        if (text.Length <= 1)
-                        {
-                            text = "0" + text;
-                        }
-
-                        //MessageBox.Show(Convert.ToString(i1), "error", MessageBoxButtons.OK);
-                        if (textBox3.Text.Length <= 1)
-                            textBox3.Text = "0" + textBox3.Text;
-
-                        textBox2.Text = textBox2.Text.Substring(0, 1).ToUpper() + textBox2.Text.Substring(1);
-
-                        var fileName = path + "\\" + textBox2.Text + " S" + textBox3.Text + "E" + text + file.Extension.ToLower();
-
-                        try
-                        {
-                            file.MoveTo(fileName);
-                            if (richTextBox2.Text == "")
-                                richTextBox2.Text = textBox2.Text + " S" + textBox3.Text + "E" + text + file.Extension.ToLower();
-                            else
-                            {
-                                richTextBox2.Text = richTextBox2.Text + "\n" + textBox2.Text + " S" + textBox3.Text + "E" + text + file.Extension.ToLower();
-                            }
-                        }
-                        catch (Exception exception)
-                        {
-                            Console.WriteLine(exception);
-                            const string errorMsg =
-                                "An error occured and didn't rename all files, make sure there isn't duplicate episodes with the same episode number.";
-                            if (!shownError)
-                                MessageBox.Show(errorMsg, title, MessageBoxButtons.OK);
-                            shownError = true;
-                        }
-
-                        i++;
-                    }
-                    else
-                    {
-                        string errorMsg = "Could not rename file " + file.Name +
-                                                " as the extension is not a media type";
-                        MessageBox.Show(errorMsg);
+                        //tv shows
+                        if (checkBox1.Checked)
+                            TvShowRename(path, file);
+                        //movies
+                        else
+                            MoviesRename(path, file);
                     }
                 }
-
             }
             else
             {
@@ -155,9 +81,131 @@ namespace GUIFilesRenamer
                 MessageBox.Show(errorMsg, title, MessageBoxButtons.OK);
             }
 
-            textBox2.Text = "";
-            textBox3.Text = "";
+            //After completion clear the tv show rename options
+            textBox2.Text = textBox3.Text = "";
+            //Re-disable rename button
+            renameButton.Enabled = false;
         }
 
+
+        //display tv show options
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Visible = label2.Visible = textBox2.Visible = textBox3.Visible = checkBox1.Checked ? true : false;
+        }
+
+        //only works for tv shows with 'E' for the episode numbering
+        private void TvShowRename(string path, FileInfo file)
+        {
+            string text = "";
+            bool foundEpisodeNum = false;
+            var i1 = 0;
+            var fileNameToSearch = file.Name.Substring(i1);
+            while (!foundEpisodeNum)
+            {
+                i1 = fileNameToSearch.IndexOf("E", StringComparison.InvariantCultureIgnoreCase);
+                text = fileNameToSearch.Substring(i1 + 1, 2);
+                foundEpisodeNum = Char.IsNumber(Convert.ToChar(text.Substring(0, 1)));
+                if (i1 == -1)
+                {
+                    string errorMsg = "Could not find episode number in file name " + file.Name;
+                    MessageBox.Show(errorMsg);
+                    return;
+                }
+                fileNameToSearch = fileNameToSearch.Substring(i1 + 2);
+            }
+            if (!Char.IsNumber(Convert.ToChar(text.Substring(1, 1))))
+                text = text.Substring(0, 1);
+            if (text.Length <= 1)
+                text = "0" + text;
+            if (textBox3.Text.Length <= 1)
+                textBox3.Text = "0" + textBox3.Text;
+
+            textBox2.Text = textBox2.Text.Substring(0, 1).ToUpper() + textBox2.Text.Substring(1);
+            var fileName = path + "\\" + textBox2.Text + " S" + textBox3.Text + "E" + text + file.Extension.ToLower();
+            RenameMethod(file, fileName);
+        }
+
+        //only works for movies with the resolution in the name
+        private void MoviesRename(string path, FileInfo file)
+        {
+                int indexOfResolution;
+                bool foundResolution = false;
+                string newName = "";
+                string restOfName = file.Name;
+                while (!foundResolution)
+                {
+                    indexOfResolution = restOfName.IndexOf("p", StringComparison.InvariantCultureIgnoreCase);
+                    if (indexOfResolution == -1)
+                        break; //error message?
+                    if (indexOfResolution - 4 > 0)
+                    {
+                        int characterToCheck = char.IsNumber(Convert.ToChar(restOfName.Substring(indexOfResolution - 4, 1)))
+                            ? indexOfResolution - 4 : indexOfResolution - 3;
+                        foundResolution = char.IsNumber(Convert.ToChar(restOfName.Substring(characterToCheck,1))) ? true : false;
+                        if (foundResolution)
+                        {
+                            newName = file.Name.Substring(0, indexOfResolution+1);
+                            break;
+                        }
+                    }
+                    restOfName = restOfName.Substring(indexOfResolution + 1);
+                    if (restOfName.Length == -1) break;
+                }
+                if (!string.IsNullOrEmpty(newName)) newName = newName.Replace(".", " ");
+                else return; //Error Message
+                var fileName = path + "\\" + newName + file.Extension.ToLower();
+                RenameMethod(file, fileName);
+        }
+
+        private void RenameMethod(FileInfo file, string fileName)
+        {
+            try
+            {
+                file.MoveTo(fileName);
+                richTextBox2.Text += fileName + "\n";
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                const string errorMsg =
+                    "An error occured and didn't rename all files, make sure there isn't duplicate episodes with the same episode number.";
+                if (!shownError)
+                    MessageBox.Show(errorMsg, title, MessageBoxButtons.OK);
+                shownError = true;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            textBox1.Text = @"C:\Users\Razzer\Desktop\test";
+            string[] filenames = Directory.GetFiles(textBox1.Text, "*", SearchOption.AllDirectories);
+
+
+            //remove dir info
+            //rename filename
+            //add dir info and extension back
+            //move file
+            foreach (var item in filenames)
+            {
+                
+                int lastBackSlash = item.LastIndexOf("\\");
+                string onlyFileName = item.Substring(lastBackSlash+1);
+                string directoryInfo = item.Substring(0, lastBackSlash + 1);
+                string extension = item.Substring(item.LastIndexOf("."));
+                onlyFileName = onlyFileName.Replace(extension,"");
+                richTextBox1.Text += "directory only : " + directoryInfo + "\n";
+                richTextBox1.Text += "filename only : " + onlyFileName + "\n";
+                richTextBox1.Text += "extension only : " + extension + "\n";
+
+                //rename onlyFileName here
+
+
+
+                //fileName = directoryInfo + reNamedFileName + extension;
+                //RenameMethod(item,fileName);
+            }
+        }
     }
 }
